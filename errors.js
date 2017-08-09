2939d9401b078dae78ab3046b248d89bde0c65e8
Ivan Ivanov <itanev@gmail.com>
2017-08-10 00:14:53 +0300

    change status codes

--- errors.js
+++ errors.js
@@ -1,10 +1,30 @@
-'use strict'
+'use strict';
+
+const status = {
+    BadRequest: 400,
+    InternalServerError: 500
+};
+
+const code = {
+
+};
 
 module.exports = {
-    ERR_SERVER_CLIENT: 1,
-    ERR_REQUEST_GENERAL: 2,
-    ERR_REQUEST_JSON: 3,
-    ERR_REQUEST_TIMEOUT: 4,
-    ERR_REQUEST_INVALID_ACTION: 5,
-    ERR_REQUEST_MISSING_ACTION: 6
+    OperationTimedOut: {
+        status: status.InternalServerError,
+        error: code
+    },
+    ParseError: {
+        status: status.BadRequest,
+        error: code
+    }
 };
+
+/*
+ ERR_SERVER_CLIENT: 1,
+ ERR_REQUEST_GENERAL: 2,
+ - ERR_REQUEST_JSON: 3,
+ - ERR_REQUEST_TIMEOUT: 4,
+ ERR_REQUEST_INVALID_ACTION: 5,
+ ERR_REQUEST_MISSING_ACTION: 6
+ */
