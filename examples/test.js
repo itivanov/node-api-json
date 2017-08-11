f3e8c6c529e53a72e4293a6da3b05ce8750382c3
Ivan Ivanov <i.ivanov@teracomm.bg>
2017-08-11 13:38:03 +0300

    implement status & error codes

--- examples/test.js
+++ examples/test.js
@@ -7,7 +7,10 @@ api.start(8000);
 api.handle('ping', ping);
 
 function ping(data) {
-    var out = {response: api.status.OK};
+    var out = {
+        response: api.status.OK,
+        data: 'pong'
+    };
 
     return out;
 }
