f1577b6dc7a61b522cc567b433fa254784a6bfff
Ivan Ivanov <itanev@gmail.com>
2017-08-12 12:25:43 +0300

    syntax fix

--- index.js
+++ index.js
@@ -1,64 +1,63 @@
-'use strict'
+'use strict';
 
 const http = require('http');
-const url = require('url');
 
 const self = module.exports = {
     status: require('./status'),
     handlers: [],
 
-    start: function(port) {
+    start: function (port) {
         var server = http.createServer();
 
         server.setTimeout(10000);
 
-        server.on('request', function(request, response) {
+        server.on('request', function (request, response) {
             var body = [];
 
-            request.on('data', function(chunk) {
+            request.on('data', function (chunk) {
                 body.push(chunk);
             });
 
-            request.on('end', function() {
+            request.on('end', function () {
                 body = Buffer.concat(body).toString();
                 self.handleRequest(request, response, body);
             });
 
-            request.on('error', function(error) {
+            request.on('error', function (error) {
                 console.log('[REQUEST] [ERR] ' + error);
                 self.responseError(response, self.status.RequestError);
             });
 
-            response.on('error', function(error) {
+            response.on('error', function (error) {
                 console.log('[RESPONSE] [ERR] ' + error);
             });
 
-            response.on('timeout', function() {
+            response.on('timeout', function () {
                 console.log('[RESPONSE] [TIMEOUT]');
                 self.responseError(response, self.status.ResponseTimeout);
             });
         });
 
-        server.on('listening', function() {
+        server.on('listening', function () {
             console.log('[SERVER] [READY]');
         });
 
-        server.on('error', function(error) {
+        server.on('error', function (error) {
             console.log('[SERVER] [ERR] ' + error);
 
             if (error.code === 'EADDRINUSE') {
                 server.close();
             } else {
-                setTimeout(function() {
+                setTimeout(function () {
                     process.exit(1);
                 }, 1000);
             }
         });
 
-        server.on('close', function() {
+        server.on('close', function () {
             console.log('[SERVER] [CLOSE]');
 
-            setTimeout(function() {
+            setTimeout(function () {
                 server.listen(port);
             }, 1000);
         });
@@ -66,7 +65,7 @@ const self = module.exports = {
         server.listen(port);
     },
 
-    handleRequest: function(request, response, body) {
+    handleRequest: function (request, response, body) {
         console.log('[REQUEST] [DATA] ' + body);
 
         try {
@@ -98,19 +97,19 @@ const self = module.exports = {
         }
     },
 
-    handleResponse: function(response, out) {
+    handleResponse: function (response, out) {
         response.setHeader('Content-Type', 'application/json');
         response.write(JSON.stringify(out));
         response.end();
     },
 
-    responseError: function(response, error) {
+    responseError: function (response, error) {
         self.handleResponse(response, {
             'response': error
         });
     },
 
-    handle: function(route, handler) {
+    handle: function (route, handler) {
         self.handlers[route] = handler;
     }
 };
