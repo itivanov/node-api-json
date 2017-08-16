bf71d23c18ab69feb323489e6f5d7e9a012217f4
Ivan Ivanov <i.ivanov@teracomm.bg>
2017-08-16 14:36:31 +0300

    add check secret

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const self = module.exports = {
 
     start: function(options) {
         if (!options.hasOwnProperty('port')) {
-            console.log('[SERVER] [ERR] Server port is not defined');
+            console.log(self.stamp() + ' [SERVER] [ERR] Server port is not defined');
             return self.shutdown();
         }
 
@@ -31,26 +31,26 @@ const self = module.exports = {
             });
 
             request.on('error', function(error) {
-                console.log('[REQUEST] [ERR] ' + error);
+                console.log(self.stamp() + ' [REQUEST] [ERR] ' + error);
                 self.responseError(response, self.status.RequestError);
             });
 
             response.on('error', function(error) {
-                console.log('[RESPONSE] [ERR] ' + error);
+                console.log(self.stamp() + ' [RESPONSE] [ERR] ' + error);
             });
 
             response.on('timeout', function() {
-                console.log('[RESPONSE] [TIMEOUT]');
+                console.log(self.stamp() + ' [RESPONSE] [TIMEOUT]');
                 self.responseError(response, self.status.ResponseTimeout);
             });
         });
 
         server.on('listening', function() {
-            console.log('[SERVER] [READY] port: ' + options.port);
+            console.log(self.stamp() + ' [SERVER] [READY] port: ' + options.port);
         });
 
         server.on('error', function(error) {
-            console.log('[SERVER] [ERR] ' + error);
+            console.log(self.stamp() + ' [SERVER] [ERR] ' + error);
 
             if (error.code === 'EADDRINUSE') {
                 server.close();
@@ -60,7 +60,7 @@ const self = module.exports = {
         });
 
         server.on('close', function() {
-            console.log('[SERVER] [CLOSE] restarting...');
+            console.log(self.stamp() + ' [SERVER] [CLOSE] restarting...');
 
             setTimeout(function() {
                 server.listen(options.port);
@@ -71,7 +71,7 @@ const self = module.exports = {
     },
 
     handleRequest: function(request, response, body) {
-        console.log('[REQUEST] [DATA] ' + body);
+        console.log(self.stamp() + ' [REQUEST] [OK] ' + body);
 
         try {
             body = JSON.parse(body);
@@ -87,7 +87,9 @@ const self = module.exports = {
             return self.responseError(response, self.status.InvalidSecret);
         }
 
-        /* TODO: check valid token */
+        if (!self.checkSecret(body.secret)) {
+            return self.responseError(response, self.status.Forbidden);
+        }
 
         if (!(body.method in self.handlers)) {
             return self.responseError(response, self.status.MissingAction);
@@ -109,13 +111,15 @@ const self = module.exports = {
     },
 
     handleResponse: function(response, out) {
+        console.log(self.stamp() + ' [RESPONSE] [OK] ' + JSON.stringify(out));
+
         response.setHeader('Content-Type', 'application/json');
         response.write(JSON.stringify(out));
         response.end();
     },
 
     responseError: function(response, error) {
-        console.log('[RESPONSE] [ERROR] ' + JSON.stringify(error));
+        console.log(self.stamp() + ' [RESPONSE] [ERROR] ' + JSON.stringify(error));
 
         self.handleResponse(response, {
             'response': error
@@ -126,9 +130,18 @@ const self = module.exports = {
         self.handlers[route] = handler;
     },
 
+    checkSecret: function(secret) {
+        // TODO: check secret in database
+        return true;
+    },
+
     shutdown: function() {
         setTimeout(function() {
             process.exit(1);
         }, 1000);
+    },
+
+    stamp: function() {
+        return new Date().toISOString();
     }
 };
--- status.js
+++ status.js
@@ -4,6 +4,7 @@ const status = {
     OK: 200,
     BadRequest: 400,
     Unauthorized: 401,
+    Forbidden: 403,
     NotFound: 404,
     NotAcceptable: 406,
     UnprocessableEntity: 422,
@@ -18,7 +19,8 @@ const error = {
     InvalidAction: 4,
     MissingAction: 5,
     ServerError: 6,
-    InvalidSecret: 7
+    InvalidSecret: 7,
+    Forbidden: 8
 };
 
 module.exports = {
@@ -52,5 +54,9 @@ module.exports = {
     InvalidSecret: {
         status: status.Unauthorized,
         error: error.InvalidSecret
+    },
+    Forbidden: {
+        status: status.Forbidden,
+        error: error.Forbidden
     }
 };
